370e94f169bbe573217a08cb5f7664147bbcf6d3
Marvelous Solomon <solomonmarvel@hotmail.com>
2024-04-05 19:56:18 +0100

    added another endpoint with /name param

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/name/<name>')
+def hello_name(name):
+    return f'Hello, {name}!'
 
 if __name__ == '__main__':
     app.run(debug=True)
